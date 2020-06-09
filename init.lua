e92ebd676902fe278abb59584a02cdcc66b87157
Tom Hartman <thomas.lees.hartman@gmail.com>
2020-06-09 17:06:38 -0400

    Connman_dbus
    Found an external library that handles grabbing service information
    and hooked it into the code. Also now can scan with appropriate
    notifications afterwards.

--- init.lua
+++ init.lua
@@ -21,185 +21,92 @@
 --- awesome-conn -- {{{
 
 --- Variables and Libraries-- {{{
-local dp        = require('dbus_proxy' )
-local wibox     = require('wibox'      )
-local gtable    = require('gears.table')
+local wibox     = require('wibox'       )
+local awful     = require('awful'       )
+local gtable    = require('gears.table' )
+local gstring   = require('gears.string')
+local connman   = require('connman_dbus')
+local serpent   = require('serpent'     )
+local naughty   = require('naughty'     )
 local ac        = {}
--- }}}
-
---- Table Helper Functions -- {{{
-
---- table.print -- {{{
-----------------------------------------------------------------------
--- 
-----------------------------------------------------------------------
-function table.print(t, level)
-   local level = level or 0
-   local pad   = ""
-   for i=1,level do
-      pad = pad .. "\t"
-   end
-
-   if type(t) == "string" then
-      print(t)
-      return 
-   end
-      
-   for i,v in pairs(t) do
-      if type(v) == "table" then
-         print(pad .. tostring(i) .. " = ")
-         table.print(v, level + 1)
-      else
-         print(pad .. tostring(i) .. " = " .. tostring(v))
-      end
-   end
-end
--- }}}
-
 -- }}}
 
 --- enums -- {{{
 local conn_state = { online = "online", idle = "idle", ready = "ready",
-                     offline = "offline"}
+                     offline = "offline", failure = "failure" }
 -- }}}
 
---- ac:currentTechnology -- {{{
-----------------------------------------------------------------------
--- Returns the current Technology in use. If there is only one
--- technology available it will return the name of that, if there are
--- mutliple it will prefer the one that has an active connection. If
--- no technologies are active it will prefer `powered' connections
--- over `unpowered' ones. If multiples of those exist it will prefer
--- whatever is first in the list of available technologies as provided
--- by connman. If no technologies are available this function will
--- return nil.
---
--- @return string, the object path of the current technology
-----------------------------------------------------------------------
-function ac:currentTech ()
+--- helper functions -- {{{
+function gstring.trim(s)
+   return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+-- }}}
 
-   -- return nil if nothing is available
-   if self._techs == nil then
-      return nil
+--- ac:scan -- {{{
+-- 
+function ac:scan ()
+   if self.scanning then
+      print("Already scanning, please wait")
+   else
+      self.scanning = true
+      awful.spawn.easy_async({ "connmanctl", "scan", "wifi" },
+         function (so, se, er, ec)
+            self.scanning = false
+            if ec ~= 0 or se ~= "" then
+               naught.notify({title = "Error Scanning for Wifi",
+                              text = "An error occured while scanning wifi: " .. error_reason ..
+                                 " " .. strerr, timeout = 0})
+            else
+               naughty.notify {title = "Scan Complete", text = "", timeout = 3}
+            end
+      end)
    end
+end
+-- }}}
 
-   -- if only one technology is available return it
-   local keys = gtable.keys(self._techs)
-   if #keys == 1 then
-      return keys[1]
-   end
+--- ac:build_connect_fn -- {{{
+-- 
+function ac:build_connect_fn (service)
+   
+end
+-- }}}
 
-   -- return the first technology that is connected
-   local retval = ""
-   for _,k in ipairs(keys) do
-      if self._techs[k].Connected == true then
-         retval = k
+--- w:update -- {{{
+----------------------------------------------------------------------
+-- Update the widget
+----------------------------------------------------------------------
+function ac:update ()
+   self._cur_service = self._connMgr.services[1]
+   
+   self._serviceInfo.text = self._cur_service.Name .. " - " ..
+      self._cur_service.Strength
+
+   self._menu_tbl = { { "Scan...", function () self:scan() end }, { "" } }
+   for k,v in pairs(self._connMgr.services) do
+      if v.Name ~= nil then
+         table.insert(self._menu_tbl, { v.Name, self:build_connect_fn(v) })
       end
    end
 
-   if retval ~= "" then
-      return retval      
-   end
+   self._menu = awful.menu( { items = self._menu_tbl } )
+   self:emit_signal("widget::updated")
+end
+-- }}}
 
-   -- return the first technology that is powered
-   for _,k in ipairs(keys) do
-      if self._techs[k].Powered == true then
-         retval = k
-      end
-   end
-
-   if retval ~= "" then
-      return retval
-   end
-
-   -- return the first technology
-   return keys[1]
+--- ac:toggle_menu -- {{{
+-- 
+function ac:toggle_menu ()
+   self._menu:toggle()
+   -- if self._menu.visible then
+   --    self._menu:hide()
+   --    self._menu.visible = false
+   -- else
+   --    self._menu.visible = true
+   --    self._menu:show()
+   -- end
 end
 -- }}}
    
---- ac:updateDisplay -- {{{
-----------------------------------------------------------------------
--- Update the display widget information
-----------------------------------------------------------------------
-function ac:updateDisplay ()
-   local tech = ac:currentTech()
-
-   self._serviceInfo.markup = self._props.State
-end
--- }}}
-
---- ac:OnMgrPropertyChanged -- {{{
-----------------------------------------------------------------------
--- 
-----------------------------------------------------------------------
-function ac:OnMgrPropsChanged (_, prop, value)
-   self._props[prop] = value
-
-   print("Entering event OnMgrPropsChanged")
-   table.print(self._props)
-
-   self:updateDisplay()
-end
--- }}}
-
---- ac:OnServicesChanged -- {{{
-----------------------------------------------------------------------
--- Update the list of currently available services in connman
---
--- @param proxy dbus_proxy object
--- @param newServices Array of string and table tuples, providing the
--- object path and properties of each new service. The properties
--- table may only contain a list of new or changed properties
--- @param oldServices Array of object paths of services that are no longer
--- available
--- return nothing
-----------------------------------------------------------------------
-function ac:OnServicesChanged (_, newServices, oldServices)
-   for _, service in ipairs(newServices) do
-      if self._services[service[1]] ~= nil then
-         gtable.crush(self._services[service[1]], service[2], true)
-      else
-         self._services[service[1]] = service[2]
-      end      
-   end
-
-   for _, service in ipairs(oldServices) do
-      self._services[service] = nil
-   end
-
-   self:updateDisplay()
-end
--- }}}
-
---- ac:OnTechAdded -- {{{
-----------------------------------------------------------------------
--- Add a technology to the list of available technologies in connman
---
--- @param objpath, the name of the technology's object path in dbus
--- @param props, table of properties of the new technology
--- @return nothing
-----------------------------------------------------------------------
-function ac:OnTechAdded (_, objpath, props)
-   self._techs[objpath] = props
-
-   self:updateDisplay()
-end
--- }}}
-
---- ac:OnTechRemoved -- {{{
-----------------------------------------------------------------------
--- Remove a technology from the list of available technologies in connman
---
--- @param objpath, the name of the technology's object path to be removed
--- @return nothing
-----------------------------------------------------------------------
-function ac:OnTechRemoved (_, objpath)
-   self._techs[objpath] = nil
-
-   self:updateDisplay()
-end
--- }}}
-
 --- new -- {{{
 ----------------------------------------------------------------------
 -- 
@@ -216,40 +123,33 @@ local function new (...)
                                   align = "center",
                                   valign = "center",
                                   widget = wibox.widget.textbox}
-   w:add(w._connIcon, w._serviceInfo)
+   w:add(w._connIcon, w._serviceInfo)   
    
    gtable.crush(w, ac, true)
+   w._connMgr = connman
 
-   w._connMgr = dp.Proxy:new( { bus       = dp.Bus.SYSTEM,
-                                name      = "net.connman",
-                                interface = "net.connman.Manager",
-                                path      = "/" })
+   -- Connect up the signals
+   connman:connect_signal(
+      function (self)
+         w:update()
+      end,
+      "PropertyChanged"
+   )
 
-   table.print(w._connMgr:GetTechnologies())
+   connman:connect_signal(
+      function (self)
+         w:update()
+      end,
+      "ServicesChanged"
+   )
 
-   -- connect to dbus signals
-   w._connMgr:connect_signal(function (...) w:OnMgrPropsChanged(...) end,
-      "PropertyChanged", nil)
-   w._connMgr:connect_signal(function (...) w:OnServicesChanged(...) end,
-      "ServicesChanged", nil)
-   w._connMgr:connect_signal(function (...) w:OnTechAdded(...)       end,
-      "TechnologyAdded", nil)
-   w._connMgr:connect_signal(function (...) w:OnTechRemoved(...)     end,
-      "TechnologyRemoved", nil)
+   -- hook up the menu
+   w:buttons(gtable.join(awful.button({}, 1,
+                            function ()
+                               w:toggle_menu() end)))
 
-
-   -- Initialize Technologies
-   local techs = w._connMgr:GetTechnologies()
-   for _, v in ipairs(techs) do
-      w._techs[v[1]] = v[2]
-   end
-
-
-   -- Initialize Manager Properties
-   w._props = w._connMgr:GetProperties()
-
-   -- Initialize the display
-   w:updateDisplay()
+   w.scanning = false
+   w:update()
    return w
 end
 -- }}}
